b3c1674e5f84001b73804ea800c221ccce7879ce
codefug <robot9917@gmail.com>
2023-11-27 03:27:51 +0900

    complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...element) {
+    for (ele of element){
+        if (array.includes(ele)){
+            array.splice(array.indexOf(ele),1);
+        }
+    }
+    return array
 };
 
 // Do not edit below this line
