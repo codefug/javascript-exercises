331b7914201c8e11f33ab057582a027253e220eb
codefug <robot9917@gmail.com>
2023-11-27 04:00:56 +0900

    complete 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+// divisable by 4 by 400 not by 100 
+// else > not leap
+if (year%4!=0 || (year%400!=0 && year%100==0)){
+    return false;
+};return true;
 };
 
 // Do not edit below this line
