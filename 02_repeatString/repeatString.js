63230ccf9ba53aac9380a814f077f8dc5356414d
codefug <robot9917@gmail.com>
2023-11-27 01:27:59 +0900

    complete 01~02 java exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string,number) {
+    let str="";
+    for (let i=0;i<number;i++){
+        str+=string;
+    }
+    return str;
 };
 
 // Do not edit below this line
