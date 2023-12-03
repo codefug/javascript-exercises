5a6fdf7eddf4d019181f2967f2258045b4c7185c
codefug <robot9917@gmail.com>
2023-12-04 01:36:28 +0900

    complete 10 exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (+n<=-1 || +n==NaN){
+        return "OOPS"
+    }
 
+    if (+n<=2){
+        return 1;
+    }
+    return +fibonacci(n-1)+fibonacci(n-2);
 };
 
 // Do not edit below this line
