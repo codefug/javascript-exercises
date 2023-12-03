10d675aeb354a8cf0510b63d3614eb30720feae5
codefug <robot9917@gmail.com>
2023-12-04 01:30:00 +0900

    complete 08 ~09 exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const processedString=str.replace(/[^a-z0-9]/gi, '').toLowerCase().split('').join('');
+    return processedString.split("").reverse().join("") == processedString;
 };
 
 // Do not edit below this line
