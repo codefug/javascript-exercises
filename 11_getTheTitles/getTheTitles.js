646bf85457fc72505fd31b9c79909b1fbcebbafc
codefug <robot9917@gmail.com>
2023-12-04 01:43:44 +0900

    complete 11 exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let result=[]
+    array.forEach((item)=>{result.push(item.title)});
+    return result;
 };
 
 // Do not edit below this line
